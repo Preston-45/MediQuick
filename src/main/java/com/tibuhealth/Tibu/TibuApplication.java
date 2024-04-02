3faeb9467312042b44ffac5d5d03beb9d04fc89e
Preston-45 <makoripreston@gmail.com>
2024-04-02 01:19:23 -0700

    Add initial project files

---
+++ src/main/java/com/tibuhealth/Tibu/TibuApplication.java
@@ -0,0 +1,13 @@
+package com.tibuhealth.Tibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TibuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tibuhealth/Tibu/TibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibuhealth.Tibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
