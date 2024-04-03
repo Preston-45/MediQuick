995392bcf41be916d3587c42a8d198115f2d749e
Preston-45 <makoripreston@gmail.com>
2024-04-03 01:30:09 -0700

    test Tibu App

--- src/main/java/com/tibuhealth/Tibu/TibuApplication.java
+++ src/main/java/com/tibuhealth/Tibu/TibuApplication.java
@@ -2,12 +2,21 @@ package com.tibuhealth.Tibu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TibuApplication {
+	public TibuApplication() {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TibuApplication.class, args);
 	}
 
+	@GetMapping({"count-users"})
+	public String getCounterUser() {
+		return "Hello Render with Spring Boot";
+	}
 }
